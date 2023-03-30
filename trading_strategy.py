451212bf196139ed3c1759ff11f4e79acc4206f1
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-03-30 11:26:48 -0700

    genesis

---
+++ trading_strategy.py
@@ -0,0 +1,4 @@
+# Use Web3.py library to interact with Metamask wallet and execute trades on Ethereum network
+# Execute trades based on optimal portfolio weights
+# Specify amounts of USDC, FTM, and WETH to buy and sell based on portfolio weights and current prices
+# Handle any errors that may occur during trading process
