0f77ce85ad2d66633eb75e5a548ca2ff07fe1125
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-03-30 11:27:40 -0700

    genesis

---
+++ backtest.py
@@ -0,0 +1,4 @@
+# Use backtesting framework like Backtrader or Zipline to test trading strategy on historical data
+# Simulate trades based on trading strategy
+# Evaluate performance of portfolio
+# Specify time range of historical data, initial balance of portfolio, and any fees or slippage that may affect trading process
