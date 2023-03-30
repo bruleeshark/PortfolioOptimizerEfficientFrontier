24210e0d88d4755b7395f9b0127caa9c81398143
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-03-30 11:24:48 -0700

    genesis

---
+++ efficient_frontier.py
@@ -0,0 +1,4 @@
+# Create efficient_frontier.py file
+# Use PyPortfolioOpt library to calculate expected returns and covariance matrix of assets
+# Calculate the efficient frontier
+# Specify any constraints on portfolio weights
