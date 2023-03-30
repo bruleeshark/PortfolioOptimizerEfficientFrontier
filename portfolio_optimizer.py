a498613eb3660de2a663aa2c53b737dacea985e7
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-03-30 11:25:52 -0700

    genesis

---
+++ portfolio_optimizer.py
@@ -0,0 +1,3 @@
+# Use optimization algorithms provided by PyPortfolioOpt library to find optimal portfolio weights
+# Find portfolio weights that maximize the Sharpe ratio
+# Specify any constraints on portfolio weights
