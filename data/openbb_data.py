6b5d9a345b03d7b723b57bc25717b492617e4c23
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-03-30 11:20:30 -0700

    genesis

---
+++ data/openbb_data.py
@@ -0,0 +1,3 @@
+# Create openbb_data.py file
+# Use OpenBB Terminal API to collect real-time data on USDC, FTM, and WETH prices and market caps
+# Store data in CSV file @ ./data/historical_data.csv
