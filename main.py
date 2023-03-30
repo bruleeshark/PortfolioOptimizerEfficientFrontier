0c13ae5a407e9e2e880d16dd6fc7f4404555b679
bruleeshark <95983960+bruleeshark@users.noreply.github.com>
2023-03-30 11:28:21 -0700

    genesis

---
+++ main.py
@@ -0,0 +1,2 @@
+# Create main.py file that imports functions from other files and orchestrates entire process of calculating efficient frontier, optimizing portfolio weights, executing trades, and backtesting strategy
+# Use command-line arguments or environment variables to specify inputs and outputs of program
